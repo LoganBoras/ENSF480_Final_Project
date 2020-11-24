7d6ffeab8e040d73b09f7fb7222b3ac156b49a65
Nurullo <thenurullo@gmail.com>
2020-11-24 16:57:11 -0700

    Base abstract Classes added, and v1 of HomePage GUI created.

---
+++ ENSF480_Final_Project/src/TRA/Presentation/Screen.java
@@ -0,0 +1,14 @@
+/* ENSF 480 - TRA Final Project 
+Group 3, November 2020
+*/
+
+package TRA.Presentation;
+
+public abstract class Screen {
+	
+	//public ArrayList<Subject>;
+	
+	abstract public void updateScreen() ;
+	
+	abstract public int displayScreen();
+}
