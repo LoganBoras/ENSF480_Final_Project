2e4db4280982d7a75d0773d59d21bf8edd2b2519
Logan Boras <60116280+LoganBoras@users.noreply.github.com>
2020-11-25 15:45:28 -0700

    Database class implemented

--- ENSF480_Final_Project/src/Database/Database.java
+++ ENSF480_Final_Project/src/Database/Database.java
@@ -1,4 +1,14 @@
 package Database;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public interface Database {
+	
+
 }
