15af5aaec115be84c223118f1db056b77893b138
Roland <rmbooth2@gmail.com>
2020-11-28 10:15:49 -0700

    Commented out all code in DatabaseManager so master compiles

--- ENSF480_Final_Project/src/TRA/Domain/TRA.java
+++ ENSF480_Final_Project/src/TRA/Domain/TRA.java
@@ -8,21 +8,12 @@ import java.util.ArrayList;
 
 public class TRA {
 
-
-    //TODO - this function should be actually implemented so it returns every
-    //movie in the database - right now it is just a dummy function
-    /**
-     * Returns array list containing upcoming movies
-     * @return ArrayList<Movie> - upcoming movies
-     */
-    public static ArrayList<Movie> upcomingMoviesList() {
-        ArrayList<Movie> movieList = new ArrayList<>();
-        movieList.add(new Movie("Cats", "yesterday", 17, "horror"));
-        return movieList;
-    }
-
     private DatabaseManager databaseManager;
     private User user;
+
+    public TRA() {
+        //TODO implement default constructor
+    }
 
     public TRA(DatabaseManager db, User u) {
         databaseManager = db;
@@ -36,4 +27,17 @@ public class TRA {
     public ArrayList<Theatre> sendTheatreList() {
         return databaseManager.getTheatres();
     }
+
+    //TODO - this function should be actually implemented so it returns every
+    //movie in the database - right now it is just a dummy function
+    /**
+     * Returns array list containing upcoming movies
+     * @return ArrayList<Movie> - upcoming movies
+     */
+    public static ArrayList<Movie> upcomingMoviesList() {
+        ArrayList<Movie> movieList = new ArrayList<>();
+        movieList.add(new Movie("Cats", "yesterday", 17, "horror"));
+        return movieList;
+    }
+
 }
